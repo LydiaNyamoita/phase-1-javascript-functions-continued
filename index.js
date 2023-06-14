a683af082ce504918588721c4a5c6965ce612786
Lydia Nyamoita <lydianyamo18@outlook.com>
2023-06-14 08:32:49 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+  }
+
+//saturdayFun("bathe my dog")
+
+console.log (saturdayFun("go to church") )
+
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork("bathe the dog"))
+
+
+
+  /*function wrapAdjective (visualFlair) {
+    return function (name = "special") {
+      return `You are ${visualFlair} ${name}!`
+    };
+  } */
+
+  function wrapAdjective(visualFlair) { return function ( name = "special") {
+      return `You are ${visualFlair}${name}${visualFlair}!`}
+    
+  }
+
+wrapAdjective("*") ("a hard worker")
+wrapAdjective("||") ("a dedicated programmer")
+
